992160a0a2b1f48fd3b94afde721603e729731ed
Perihanceliko <pericelikoglu@gmail.com>
2026-03-29 00:14:09 +0300

    data_loader geliştirildi ve proje setup düzenlendi

--- src/data_loader.py
+++ src/data_loader.py
@@ -3,8 +3,18 @@ import tensorflow as tf
 def veri_hazirla(data_yolu):
     # Bu fonksiyon Merve ve Ayşe veri setini yüklediğinde 
     # resimleri modele pompalayacak.
-    print(f"--- {data_yolu} konumundaki veriler okunmaya hazır! ---")
-    pass
+    print(f"--- {data_yolu} klasörü taranıyor...")
+    
+    dataset = tf.keras.preprocessing.image_dataset_from_directory(
+        data_yolu,
+        image_size=(224, 224),
+        batch_size=32
+    )
+
+    print("Veri başarıyla yüklendi!")
+    print("Sınıflar:", dataset.class_names)
+
+    return dataset
 
 if __name__ == "__main__":
-    veri_hazirla("../dataset")
+    veri_hazirla("dataset")
