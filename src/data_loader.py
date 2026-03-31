307683163cdc1e326006ed8f967f440ff1bf16f8
Perihanceliko <pericelikoglu@gmail.com>
2026-03-31 16:55:38 +0300

    Sprint1: AI pipeline ve prediction sistemi tamamlandı

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,20 +1,30 @@
 import tensorflow as tf
 
-def veri_hazirla(data_yolu):
-    # Bu fonksiyon Merve ve Ayşe veri setini yüklediğinde 
-    # resimleri modele pompalayacak.
-    print(f"--- {data_yolu} klasörü taranıyor...")
+def veri_hazirla():
+
+    print("Train ve Validation verileri yükleniyor...")
     
-    dataset = tf.keras.preprocessing.image_dataset_from_directory(
-        data_yolu,
+    train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+        "dataset/train",
+        validation_split=0.2,
+        subset="training",
+        seed=123,
         image_size=(224, 224),
         batch_size=32
     )
 
-    print("Veri başarıyla yüklendi!")
-    print("Sınıflar:", dataset.class_names)
+    val_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+        "dataset/train",
+        validation_split=0.2,
+        subset="validation", 
+        seed=123,
+        image_size=(224, 224),
+        batch_size=32
+    )
+    print("Sınıflar:", train_dataset.class_names)
 
-    return dataset
+    return train_dataset, val_dataset
+   
 
 if __name__ == "__main__":
-    veri_hazirla("dataset")
+  veri_hazirla()
---
+++ src/predict.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+# modeli yükle
+model = tf.keras.models.load_model("models/model.h5")
+
+# class isimleri (data_loader ile aynı sırada olmalı)
+class_names = [
+    'Pepper,_bell___Bacterial_spot',
+    'Pepper,_bell___healthy',
+    'Potato___Early_blight',
+    'Potato___Late_blight',
+    'Potato___healthy',
+    'Tomato___Bacterial_spot',
+    'Tomato___Early_blight'
+]
+
+def predict_image(image_path):
+
+    img = Image.open(image_path)
+    img = img.resize((224, 224))
+
+    img = np.array(img) / 255.0
+    img = np.expand_dims(img, axis=0)
+
+    prediction = model.predict(img)
+
+    predicted_class = np.argmax(prediction)
+    confidence = np.max(prediction)
+
+    return {
+        "disease": class_names[predicted_class],
+        "confidence": float(confidence)
+    }
+
+
+if __name__ == "__main__":
+    result = predict_image("testimage.jpg")  # buraya test foto koyacağız
+    print(result)
